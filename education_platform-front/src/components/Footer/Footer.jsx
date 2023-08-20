593466a1d8d39e42cf9bf72a3ccf9fe1341ca4cf
MazenSghaier <sghaiermazen7@gmail.com>
2023-08-20 15:24:32 +0100

    15th commit 8/20 nice footer baby

--- education_platform-front/src/components/Footer/Footer.jsx
+++ education_platform-front/src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import {  Typography } from "@mui/material";
 import { RiPantoneLine } from "react-icons/ri";
 
-import "./footer.css";
+import youTube from '../../assests/Social_media/youtube.png';
+import twitter from '../../assests/Social_media/twitter.png';
+import facebook from '../../assests/Social_media/facebook.png';
+import linkedin from '../../assests/Social_media/linkedin.png';
+import instagram from '../../assests/Social_media/instagram.png';
 
 const footerQuickLinks = [
   {
@@ -47,69 +51,69 @@ const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="row-cols-lg-4">
+      <Row className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0 p-3"> 
           <Col lg="3" md="6" className="mb-4">
-          <Typography variant="h5" className="flex items-center gap-1">
-              <RiPantoneLine className="text-xl" style={{ animation: "slideFromBottom 1s ease-out" }} />
+            <Typography variant="h5" className="flex items-center ">
+              <RiPantoneLine className="text-xl animate-slideFromBottom" />
               <span className="animate__animated animate__slideInRight">Learners.</span>
             </Typography>
           </Col>
 
           <Col lg="3" md="6" className="mb-4">
-            <h6 className="fw-bold">Explore</h6>
+            <h6 className="font-medium text-cyan-400">Explore</h6>
             <ListGroup className="link__list">
               {footerQuickLinks.map((item, index) => (
                 <ListGroupItem key={index} className="border-0 ps-0 link__item">
-                  <a href={item.url}>{item.display}</a>
+                  <a className="hover:text-cyan-400" href={item.url}>{item.display}</a>
                 </ListGroupItem>
               ))}
             </ListGroup>
           </Col>
 
           <Col lg="3" md="6" className="mb-4">
-            <h6 className="fw-bold">Information</h6>
+            <h6 className="font-medium text-cyan-400">Information</h6>
             <ListGroup className="link__list">
               {footerInfoLinks.map((item, index) => (
                 <ListGroupItem key={index} className="border-0 ps-0 link__item">
-                  <a href={item.url}>{item.display}</a>
+                  <a className="hover:text-cyan-400 " href={item.url}>{item.display}</a>
                 </ListGroupItem>
               ))}
             </ListGroup>
           </Col>
 
           <Col lg="3" md="6" className="mb-4">
-            <h6 className="fw-bold">Get in Touch</h6>
-            <p>Address: Sylhet, Bangladesh</p>
+            <h6 className="font-medium text-cyan-400">Get in Touch</h6>
+            <p className="">Address: Sylhet, Bangladesh</p>
             <p>Phone: +88 0123456789</p>
             <p>Email: example@gmail.com</p>
           </Col>
         </Row>
 
-        
-        <div className="follows">
-              <p className="mb-0">Follow us on social media</p>
-              <span>
-                <a href="facebook.com">
-                  <i className="ri-facebook-line"></i>
+         
+        <Row className="justify-center items-center mt-4">
+          <Col lg="12" md="12" className="mb-4 text-center">
+            <div className="flex flex-col space-y-4 mx-auto">
+              <h6 className="font-medium text-sm">Follow us on Social Media</h6>
+              <div className="flex justify-center space-x-4">
+                <a href="/">
+                  <img alt="" src={facebook} className="w-15 h-15" />
                 </a>
-              </span>
-              <span>
-                <a href="facebook.com">
-                  <i className="ri-instagram-line"></i>
+                <a href="/">
+                  <img alt="" src={youTube} className="w-15 h-15" />
                 </a>
-              </span>
-              <span>
-                <a href="facebook.com">
-                  <i className="ri-linkedin-line"></i>
+                <a href="/">
+                  <img alt="" src={twitter} className="w-15 h-15" />
                 </a>
-              </span>
-              <span>
-                <a href="facebook.com">
-                  <i className="ri-twitter-line"></i>
+                <a href="/">
+                  <img alt="" src={linkedin} className="w-15 h-15" />
                 </a>
-              </span>
+                <a href="/">
+                  <img alt="" src={instagram} className="w-15 h-15" />
+                </a>
+              </div>
             </div>
-            
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
