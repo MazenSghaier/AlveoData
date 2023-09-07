fd35a2b8303dfeef3cc802b733237c95af33d09c
MazenSghaier <sghaiermazen7@gmail.com>
2023-09-07 19:48:36 +0100

    35th commit 9/7 preparing backend

--- education_platform-back/src/Routes/users.js
+++ education_platform-back/src/Routes/users.js
@@ -1,26 +1,24 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router();
 
-const users =[
-    {
-        FirstName:"John",
-        LasttName:"Rox",
-        age:25
-    },
-    {
-        FirstName:"Ali",
-        LasttName:"imen",
-        age:8
-    }
-]
+const users =[]
 
 router.get("/", (req,res) => {
     res.send(users);
 });
 
 router.post('/',(req,res) => {
-    console.log('POST ROUTE REACHED')
+    const user = req.body;
+
+    users.push({ ...user, id:uuidv4() });
+
+    res.send(`User with the name ${user.FirstName} added to the database!`);
 });
+
+router.get('/:id',(req,res) => {
+    res.send('THE GET ID ROUT')
+})
 
 export default router ;
